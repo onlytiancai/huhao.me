3f09a06ca0a3e0ed8eb54ce07f869c232dd63899
huhao <onlytiancai@gmail.com>
2012-03-24 15:01:26 +0800

    使用小天天博客重新上线

---
+++ README.md
@@ -0,0 +1,5 @@
+## huhao.me
+
+我的博客，使用[小天天博客](https://github.com/onlytiancai/xiaotiantian)驱动
+
+
---
+++ meta.js
@@ -0,0 +1,15 @@
+{
+    "site_name": "huhao.me",
+    "copyright": "wawasoft 2012",
+    "cates": [
+        {"name": "tech", "text":"技术"},
+        {"name": "art", "text":"艺术"},
+        {"name": "other", "text":"其它"}
+    ],
+    "articles":[
+        {"title": "哪段代码更Pythonic呢？", "file": "2012-03-02-witch-one-is-best-pythonic", "cate":"tech"},
+        {"title": "发一组人像", "file": "2012-03-17-portrait-5018", "cate":"art"},
+        {"title": "发一组猫咪照", "file": "2012-03-20-cat-5018", "cate":"art"},
+        {"title": "微博摘录2012-02", "file": "2012-02-28-weibo", "cate":"other"}
+        ]
+}
