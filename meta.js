c235dbbb2365a98ad03df7ec2d40d31d97a21298
huhao <onlytiancai@gmail.com>
2012-09-18 11:42:46 +0800

    post update

--- meta.js
+++ meta.js
@@ -10,7 +10,7 @@
         {"title": "哪段代码更Pythonic呢？", "file": "2012-03-02-witch-one-is-best-pythonic", "cate":"tech"},
         {"title": "发一组人像", "file": "2012-03-17-portrait-5018", "cate":"art"},
         {"title": "发一组猫咪照", "file": "2012-03-20-cat-5018", "cate":"art"},
-        {"title": "微博摘录2012-02", "file": "2012-02-28-weibo", "cate":"other"}
+        {"title": "微博摘录2012-02", "file": "2012-02-28-weibo", "cate":"other"},
         {"title": "一组好听的英文歌曲", "file": "2012-09-18-music", "cate":"art"}
         ]
 }
