1436a29b1b534d97b49a032e38d6bd71e9140688
huhao <huhao@dnspod.com>
2013-08-29 08:42:09 +0800

    todo change

--- meta.js
+++ meta.js
@@ -12,7 +12,7 @@
         {"title": "发一组猫咪照", "file": "2012-03-20-cat-5018", "cate":"art"},
         {"title": "微博摘录2012-02", "file": "2012-02-28-weibo", "cate":"other"},
         {"title": "一组好听的英文歌曲", "file": "2012-09-18-music", "cate":"art"},
-        {"title": "2012年技术学习总结", "file": "2013-01-02-summary", "cate":"tech"}
+        {"title": "2012年技术学习总结", "file": "2013-01-02-summary", "cate":"tech"},
         {"title": "todo", "file": "2013-08-29-todo", "cate":"tech"}
         ]
 }
